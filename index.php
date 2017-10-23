80882d9bb7d5f408b25fbfe5f80c96a3e6ee1887
Eric H. Garcia <erichenriquegarcia@MacBook-Pro-de-Eric.local>
2017-10-23 09:56:42 -0200

    mail server

--- index.php
+++ index.php
@@ -140,32 +140,160 @@
 //	echo "<p> Please enter a positive number </p>";
 //}
 //print_r($_POST);
-$arrayNames = ["Eric", "Aline", "Helena"];
+//$arrayNames = ["Eric", "Aline", "Helena"];
+//
+//if ($_POST) {
+//	$isKnown = false;
+//	foreach($arrayNames as $value) {
+//		if ($value == $_POST['name']) {
+//			$isKnown = true;
+//		}
+//	}
+//	
+//	if($isKnown) {
+//		echo "Hi there ".$_POST['name']."!";
+//	}else {
+//		echo "I dont't know you";
+//	}
+//	
+//}
+
+//$emailTo = "dericsp@gmail.com";
+//
+//$subject = "I hope this works!";
+//
+//$body = "I think you're great!";
+//
+//$headers = "from: dericsp@gmail.com";
+//
+//if (mail($emailTo, $subject, $body, $headers)) {
+//	echo "The email was sent sucessfuly";
+//}else {
+//	echo "The email could not be sent";
+//	
+//}
+	$error = "";
+	$sucessMessage = "";
 
 if ($_POST) {
-	$isKnown = false;
-	foreach($arrayNames as $value) {
-		if ($value == $_POST['name']) {
-			$isKnown = true;
-		}
+	
+	if(!$_POST["email"]) {
+		$error .= "An email address is requeired<br>";
 	}
 	
-	if($isKnown) {
-		echo "Hi there ".$_POST['name']."!";
-	}else {
-		echo "I dont't know you";
+	if(!$_POST["content"]) {
+		$error .= "A content is requeired<br>";
 	}
 	
+	if(!$_POST["subject"]) {
+		$error .= "A subject  is requeired<br>";
+	}
+	
+	
+if ($_POST['email'] && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) == false) {
+	
+		$error .= "The email address is invalid<br>";
 }
-
+	if ($error != "") {
+		$error = '<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form</strong></p>'.$error.' </div>';
+	} else {
+		
+		$emailTo = "dericsp@gmail.com";
+		
+		$subject = $_POST['subject'];
+		
+		$content = $_POST['content'];
+		
+		$headers = "from ".$_POST['email'];
+			
+			if (mail($emailTo, $subject, $content, $headers)) {
 	
+				$sucessMessage = '<div class="alert alert-success" role="alert"><p><strong>Your message was sent.</strong></p>';
+}else {
+	$error = '<div class="alert alert-danger" role="alert"><p><strong>Your message couldn\'t be sent - please try again later.</strong></p>';	
+}
+	}
+}
 
 
 ?>
 
+	<!DOCTYPE html>
+	<html lang="en">
 
-	<p>Hello What's your name?</p>
-<form method="post">
-	<input name="name" type="text">
-	<input type="submit" value="Go!">
-</form>
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	</head>
+
+	<body>
+		<div class="container">
+			<h1>Get in Touch!</h1>
+
+			<div id="error">
+				<? echo $error.$sucessMessage; ?>
+			</div>
+
+			<form method="post">
+				<div class="form-group">
+					<label for="email">Email address</label>
+					<input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Your email">
+					<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+				</div>
+				<div class="form-group">
+					<label for="subject">Subject</label>
+					<input type="text" class="form-control" id="subject" name="subject">
+				</div>
+				<div class="form-group">
+					<label for="exampleTextarea">What you like to ask us?</label>
+					<textarea class="form-control" id="content" name="content" rows="3"></textarea>
+				</div>
+				<button type="submit" id="submit" class="btn btn-primary">Submit</button>
+			</form>
+		</div>
+
+
+
+
+		<!-- jQuery first, then Tether, then Bootstrap JS. -->
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+		<script type="text/javascript">
+			$("form").submit(function(e) {
+				e.preventDefault();
+
+				var error = "";
+
+				if ($("#email").val() == "") {
+					error += "The email field is required<br>";
+				}
+
+				if ($("#subject").val() == "") {
+					error += "The subject field is required<br>";
+				}
+
+				$("#error").html(error);
+
+				if ($("#content").val() == "") {
+					error += "The content field is required";
+				}
+
+				if (error != "") {
+
+					$("#error").html('<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form</strong></p>' + error + ' </div>');
+
+				} else {
+					$("form").off("submit").submit();
+				}
+			});
+
+		</script>
+	</body>
+
+	</html>
