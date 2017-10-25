d1841dbd53a196816d298965be6e2c44b972df4b
dericsp <dericsp@gmail.com>
2017-10-25 06:48:06 -0200

    weather app

---
+++ included.php
@@ -0,0 +1,4 @@
+<?php 
+//	echo "I'm an included file";
+//echo file_get_contents("https://www.weather-forecast.com/locations/New-York/forecasts/latest")
+?>
--- index.php
+++ index.php
@@ -172,62 +172,70 @@
 //	echo "The email could not be sent";
 //	
 //}
-	$error = "";
-	$sucessMessage = "";
+//	$error = "";
+//	$sucessMessage = "";
+//
+//    if ($_POST) {
+//	
+//	   if(!$_POST["email"]) {
+//		$error .= "An email address is requeired<br>";
+//	   }
+//	
+//	   if(!$_POST["content"]) {
+//		$error .= "A content is requeired<br>";
+//	   }
+//	
+//	   if(!$_POST["subject"]) {
+//		$error .= "A subject  is requeired<br>";
+//	   }
+//	
+//	
+//        if ($_POST['email'] && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) == false) {
+//	
+//		$error .= "The email address is invalid<br>";
+//        }
+//    
+//	   if ($error != "") {
+//		  $error = '<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form</strong></p>'.$error.' </div>';
+//	   } else {
+//		
+//		$emailTo = "dericsp@gmail.com";
+//		
+//		$subject = $_POST['subject'];
+//		
+//		$content = $_POST['content'];
+//		
+//		$headers = "from ".$_POST['email'];
+//			
+//			if (mail($emailTo, $subject, $content, $headers)) {
+//	
+//				$sucessMessage = '<div class="alert alert-success" role="alert"><p><strong>Your message was sent.</strong></p>';
+//            }else {
+//                $error = '<div class="alert alert-danger" role="alert"><p><strong>Your message couldn\'t be sent - please try again later.</strong></p>';	
+//            }
+//       }
+//    }
 
-    if ($_POST) {
-	
-	   if(!$_POST["email"]) {
-		$error .= "An email address is requeired<br>";
-	   }
-	
-	   if(!$_POST["content"]) {
-		$error .= "A content is requeired<br>";
-	   }
-	
-	   if(!$_POST["subject"]) {
-		$error .= "A subject  is requeired<br>";
-	   }
-	
-	
-        if ($_POST['email'] && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) == false) {
-	
-		$error .= "The email address is invalid<br>";
-        }
-    
-	   if ($error != "") {
-		  $error = '<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form</strong></p>'.$error.' </div>';
-	   } else {
-		
-		$emailTo = "dericsp@gmail.com";
-		
-		$subject = $_POST['subject'];
-		
-		$content = $_POST['content'];
-		
-		$headers = "from ".$_POST['email'];
-			
-			if (mail($emailTo, $subject, $content, $headers)) {
-	
-				$sucessMessage = '<div class="alert alert-success" role="alert"><p><strong>Your message was sent.</strong></p>';
-            }else {
-                $error = '<div class="alert alert-danger" role="alert"><p><strong>Your message couldn\'t be sent - please try again later.</strong></p>';	
-            }
-       }
-    }
+include("included.php");
 
+//echo file_get_contents("https://www.bubalao.com.br");
 
 ?>
 
+	<!--
 	<!DOCTYPE html>
 	<html lang="en">
 
 	<head>
-		<!-- Required meta tags -->
+-->
+	<!-- Required meta tags -->
+	<!--
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+-->
 
-		<!-- Bootstrap CSS -->
+	<!-- Bootstrap CSS -->
+	<!--
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 	</head>
 
@@ -256,11 +264,13 @@
 				<button type="submit"  id="submit" class="btn btn-primary">Submit</button>
 			</form>
 		</div>
+-->
 
 
 
 
-		<!-- jQuery first, then Tether, then Bootstrap JS. -->
+	<!-- jQuery first, then Tether, then Bootstrap JS. -->
+	<!--
 		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
@@ -296,6 +306,78 @@
 			});
 
 		</script>
+	</body>
+
+	</html>
+-->
+
+<?php 
+	if ($_GET['city']) {
+		$forecastPage = file_get_contents("http://www.weather-forecast.com/locations/London/forecasts/latest");
+		
+		$pageArray = explode('3 Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">', $forecastPage);
+		
+		$secondPageArray = explode('</span></span></span>', $pageArray[1]);
+		echo $secondPageArray[0];
+	}
+?>
+	<!doctype html>
+	<html lang="en">
+
+	<head>
+		<title>Weather Scraper</title>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	</head>
+
+	<body>
+
+		<style type="text/css">
+			html {
+				background: url(background.jpeg) no-repeat center center fixed;
+				-webkit-background-size: cover;
+				-moz-background-size: cover;
+				-o-background-size: cover;
+				background-size: cover;
+			}
+
+			body {
+				background: none;
+			}
+
+			.container {
+				text-align: center;
+				margin-top: 200px;
+				width: 450px;
+			}
+			
+			input {
+				margin: 20px 0;
+			}
+
+		</style>
+		<div class="container">
+			<h1>What's the Weather ?</h1>
+			
+
+			<form >
+				<div class="form-group">
+					<label for="city">Enter the name of a city.</label>
+					<input type="text" class="form-control" id="city" name="city" placeholder="Ex. London, Tokyo">
+				</div>
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
+		</div>
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 	</body>
 
 	</html>
